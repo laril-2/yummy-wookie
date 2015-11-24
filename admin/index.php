03818baa9149192b4b478f71ebc6f3e494c4b0cd
laril-2 <laril@viidakko.fi>
2015-11-24 22:05:16 +0200

    SSL pakotus

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,14 @@
 <?php
 
+	// force SSL
+	if ($_SERVER['HTTPS'] != 'on')	{
+		header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		exit();
+	}
+
 	require '../includes/config.php';
 	require '../includes/functions.php';
-			
+	
 	$uri = $_SERVER['REQUEST_URI'];
 	
 	$uri = explode('?', $uri)[0];
