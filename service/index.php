ccfbccbd55b9bf567de490a7c74c1728607f594b
laril-2 <laril@viidakko.fi>
2016-01-29 15:01:03 +0200

    basic controller/view system

--- service/index.php
+++ service/index.php
@@ -3,8 +3,18 @@
 require_once 'includes/config.php';
 require_once 'includes/functions.php';
 
-getDB();
+$uri = $_SERVER['REQUEST_URI'];
 
+if (substr($uri, 0, 11) === '/index.php/' && strlen($uri) > 11)	{
+	$subs = explode('/', $uri);
+	$uri = implode('/', array_slice($subs, 2));
+	$uri = str_replace('.', '', $uri);
 
-
-echo '<p>' . __FILE__ . "</p>\n";
+	if (file_exists(BASE_PATH . "/views/{$uri}.php")) {
+		include BASE_PATH . "/views/{$uri}.php";
+	} else {
+		echo '<h1>VIEW NOT FOUND</p>';
+	}
+} else {
+	include BASE_PATH . '/views/list_files.php';
+}
---
+++ service/views/list_files.php
@@ -0,0 +1,8 @@
+<?php
+	getDB();
+
+	var_dump($db);
+
+?>
+
+<h1>FILES LIST</h1>
